5fede6ea073bd448ab2859a3e0cbcf97671c94a7
Philippe Hansen <philippe@Philippes-MBP.attlocal.net>
2025-01-16 19:01:02 -0600

    Website Fix

--- vitok/models/perceptual_networks/__init__.py
+++
@@ -1,2 +0,0 @@
-from vitok.models.perceptual_networks.discriminator import Discriminator, hinge_d_loss, compute_lecam_loss, NLayerDiscriminator, NLayerDiscriminator3D
-from vitok.models.perceptual_networks.lpips import LPIPS
